85882bf97a16519df4c859d7504879de8fd333d9
Ayush2025 <88815689+Ayush2025@users.noreply.github.com>
2025-06-14 08:03:19 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,36 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import AuthProvider from './context/AuthContext';
-import Login from './components/Login';
-import Verify from './pages/Verify';
+import React, { useContext } from 'react'
+import { Routes, Route } from 'react-router-dom'
 
-export default function App() {
+import Home from './pages/Home'
+import Result from './pages/Result'
+import BuyCredit from './pages/BuyCredit'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Login from './components/Login'
+import { AppContext } from './context/AppContext'
+import Verify from './pages/Verify'
+
+const App = () => {
+
+  const { showLogin } = useContext(AppContext)
+
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/verify" element={<Verify />} />
-          {/* ... rest of your pages ... */}
-        </Routes>
-      </BrowserRouter>
-    </AuthProvider>
-  );
+    <div className='px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-50 to-orange-50 '>
+      <ToastContainer position='bottom-right' />
+      <Navbar />
+      {showLogin && <Login />}
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/result' element={<Result />} />
+        <Route path='/buy' element={<BuyCredit />} />
+        <Route path='/verify' element={<Verify />} />
+      </Routes>
+      <Footer />
+    </div>
+  )
 }
+
+export default App

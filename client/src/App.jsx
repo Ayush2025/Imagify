81df8a51c3cf46d12db4996f584bfcdd06de5413
Ayush2025 <88815689+Ayush2025@users.noreply.github.com>
2025-06-14 07:58:11 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,36 +1,19 @@
-import React, { useContext } from 'react'
-import { Routes, Route } from 'react-router-dom'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AuthProvider from './context/AuthContext';
+import Login from './components/Login';
+import Verify from './pages/Verify';
 
-import Home from './pages/Home'
-import Result from './pages/Result'
-import BuyCredit from './pages/BuyCredit'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import Login from './components/Login'
-import { AppContext } from './context/AppContext'
-import Verify from './pages/Verify'
-
-const App = () => {
-
-  const { showLogin } = useContext(AppContext)
-
+export default function App() {
   return (
-    <div className='px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-50 to-orange-50 '>
-      <ToastContainer position='bottom-right' />
-      <Navbar />
-      {showLogin && <Login />}
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/result' element={<Result />} />
-        <Route path='/buy' element={<BuyCredit />} />
-        <Route path='/verify' element={<Verify />} />
-      </Routes>
-      <Footer />
-    </div>
-  )
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/verify" element={<Verify />} />
+          {/* ... rest of your pages ... */}
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  );
 }
-
-export default App

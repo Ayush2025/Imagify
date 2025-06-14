0e1eeb557ec193e1daa085be53cfce6ed83c47ba
Ayush2025 <88815689+Ayush2025@users.noreply.github.com>
2025-06-14 05:48:17 +0530

    Update userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,4 +1,3 @@
-// server/routes/userRoutes.js
 import express from 'express';
 import {
   registerUser,
@@ -9,19 +8,16 @@ import {
   paymentStripe,
   verifyStripe,
 } from '../controllers/UserController.js';
-import authUser from '../middlewares/auth.js'
+import authUser from '../middlewares/auth.js';
 
 const userRouter = express.Router();
 
-// Public
 userRouter.post('/register', registerUser);
-userRouter.post('/login',    loginUser);
-
-// Protected
-userRouter.get(  '/credits',      authUser, userCredits);
-userRouter.post( '/pay-razor',    authUser, paymentRazorpay);
-userRouter.post( '/verify-razor', authUser, verifyRazorpay);
-userRouter.post( '/pay-stripe',   authUser, paymentStripe);
-userRouter.post( '/verify-stripe',authUser, verifyStripe);
+userRouter.post('/login', loginUser);
+userRouter.get('/credits', authUser, userCredits);
+userRouter.post('/pay-razor', authUser, paymentRazorpay);
+userRouter.post('/verify-razor', verifyRazorpay);
+userRouter.post('/pay-stripe', authUser, paymentStripe);
+userRouter.post('/verify-stripe', authUser, verifyStripe);
 
 export default userRouter;

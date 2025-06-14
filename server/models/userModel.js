f8f03aa3583e0842f8307720b66ad4e86d4558f2
Ayush2025 <88815689+Ayush2025@users.noreply.github.com>
2025-06-14 07:55:27 +0530

    Update userModel.js

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,12 +1,10 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-const userSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    creditBalance: { type: Number, default: 5 },
-})
+const UserSchema = new mongoose.Schema({
+  name: String,
+  email: { type: String, unique: true },
+  password: String,
+  creditBalance: { type: Number, default: 0 }
+});
 
-const userModel = mongoose.models.user || mongoose.model("user", userSchema)
-
-export default userModel;
+export default mongoose.model('User', UserSchema);

4224d54a3fd7c4f0f825bc3471fc1aa639d9d838
Ayush2025 <88815689+Ayush2025@users.noreply.github.com>
2025-06-14 08:03:36 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-import 'react-toastify/dist/ReactToastify.css';
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import AppContextProvider from './context/AppContext.jsx'
 
-ReactDOM.createRoot(document.getElementById('root'))
-  .render(<App />);
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <AppContextProvider>
+      <App />
+    </AppContextProvider>
+  </BrowserRouter>,
+)
